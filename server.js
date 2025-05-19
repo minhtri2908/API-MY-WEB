b7aba378729dbe71b88d1fde2df40282a795d225
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2025-05-19 15:22:41 +0700

    fix link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: "tri-pham-indol.vercel.app", 
+  origin: "https://tri-pham-indol.vercel.app/", 
   credentials: true,              
 }));
 app.use(express.json());
